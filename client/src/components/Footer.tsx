89c0fd858be67e72ff4c7b5a30f381a250d6ba7a
Ann <ann.suparada@gmail.com>
2023-10-23 16:31:01 -0700

    add social media links to footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -6,6 +6,8 @@ import LaLogo from '../logo/los-angeles.svg'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import InstagramIcon from '@mui/icons-material/Instagram'
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import { Link } from 'react-router-dom'
 
 const Footer: React.FC = () => {
   const styles = {
@@ -55,9 +57,30 @@ const Footer: React.FC = () => {
               phone: 323.612.8138
             </p>
             <div>
-              <LinkedInIcon style={styles.icon} />
-              <AlternateEmailIcon style={styles.icon} />
-              <InstagramIcon style={styles.icon} />
+              <Link
+                to="https://www.linkedin.com/company/hi-mid-low/"
+                target="_blank"
+              >
+                <LinkedInIcon style={styles.icon} />
+              </Link>
+              <Link
+                to="https://www.threads.net/@himidlow_agency"
+                target="_blank"
+              >
+                <AlternateEmailIcon style={styles.icon} />
+              </Link>
+              <Link
+                to="https://www.instagram.com/himidlow_agency/"
+                target="_blank"
+              >
+                <InstagramIcon style={styles.icon} />
+              </Link>
+              <Link
+                to="https://www.facebook.com/profile.php?id=61552490501398"
+                target="_blank"
+              >
+                <FacebookIcon style={styles.icon} />
+              </Link>
             </div>
           </div>
         </div>
