6d2e7b7ad6cc0294a7ce7a2528a012dc7cea19ae
Ann <ann.suparada@gmail.com>
2023-10-23 15:53:37 -0700

    refactor data

--- client/src/data.ts
+++ client/src/data.ts
@@ -121,3 +121,62 @@ export const brandingListLastRow = [
     width: '90px',
   },
 ]
+
+export const membershipPerks = [
+  {
+    title: 'Sound Board',
+    description: 'Add as many unique projects to your board as you want.',
+  },
+  {
+    title: 'Fast and Easy',
+    description:
+      'You’ll get your mixes back in 1 to 3 days.  It sure makes scheduling easy!',
+  },
+  {
+    title: 'Fix Flat Fee',
+    description:
+      'All the mixes you need at a consistent flat price.  No more negotiating!',
+  },
+  {
+    title: 'World Class Sound',
+    description:
+      'We’ve got over 20 years experience editing, designing and mixing sound for major Hollywood studios.',
+  },
+  {
+    title: 'Scaling for any need',
+    description:
+      'As your needs grow, so can your plan. Change plans at any time for any reason.',
+  },
+  {
+    title: 'Cancel anytime',
+    description:
+      'All good relationships need to take a breather every now and again. That’s why you can cancel or pause your subscription at any time.',
+  },
+  {
+    title: '',
+    description: '',
+  },
+  {
+    title: 'You’re one of a small cohort',
+    description:
+      'We only take on 6 active clients, no more! Building powerful relationships with our clients is point number one.',
+  },
+]
+
+export const details = [
+  {
+    title: 'Short calls or no meetings at all!',
+    description:
+      'When you’ve got a mix that needs to get done fast, the last thing you want is to try and schedule a call. If you need it, we’re there for you, but if not, you can leave notes and references in your project folder for us to review.',
+  },
+  {
+    title: 'Manage your projects with Trello',
+    description:
+      'Add as many projects to the backlog as you’d like. When your project is being worked on, we’ll move it to “In progress”, then to “To Review” when it’s ready for you! Keep track of all this real time!',
+  },
+  {
+    title: 'Keep the entire team involved',
+    description:
+      'You can add as many team members to your project as you’d like.',
+  },
+]
