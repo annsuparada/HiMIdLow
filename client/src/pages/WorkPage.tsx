38a82318c6d0d4c09d4035bf7655b0b470acf31e
Ann <ann.suparada@gmail.com>
2023-10-25 00:50:38 -0700

    work page mobile

--- client/src/pages/WorkPage.tsx
+++ client/src/pages/WorkPage.tsx
@@ -1,18 +1,22 @@
-import { Button } from '@mui/material'
+import { Button, useMediaQuery } from '@mui/material'
 import React from 'react'
 import MainWrapper from '../components/MainWrapper'
 import NavBar from '../components/NavBar'
 import ScrollToTop from '../components/ScrollToTop'
 import VideoModal from '../components/VideoModal'
 import { videoListWorkPage } from '../data'
-import { background } from '../theme'
+import { background, mobileView, tabletView } from '../theme'
 
 const WorkPage: React.FC = () => {
+  const isTablet = useMediaQuery(`(max-width:${tabletView})`)
+  const isMobile = useMediaQuery(`(max-width:${mobileView})`)
+
   const styles = {
     container: {
       display: 'grid',
       justifyContent: 'center',
       marginTop: '2rem',
+      padding: '10px',
     },
     textContainer: {
       maxWidth: '600px',
@@ -20,7 +24,11 @@ const WorkPage: React.FC = () => {
     },
     imgContainer: {
       display: 'grid',
-      gridTemplateColumns: 'repeat(4, 1fr)',
+      gridTemplateColumns: isMobile
+        ? '1fr'
+        : isTablet
+        ? '1fr 1fr'
+        : 'repeat(4, 1fr)',
       gap: '10px',
     },
   }
