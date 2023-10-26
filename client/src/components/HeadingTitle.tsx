36d143ed07c529393659b99e25aac4c99d17740e
Ann <ann.suparada@gmail.com>
2023-10-26 15:53:11 -0700

    fixed heading title

--- client/src/components/HeadingTitle.tsx
+++ client/src/components/HeadingTitle.tsx
@@ -1,29 +1,36 @@
+import { useMediaQuery } from '@mui/material'
 import React from 'react'
+import { desktopView, mobileView, tabletView } from '../theme'
 
 interface HadingTitleProps {
   title: string
 }
 const HadingTitle: React.FC<HadingTitleProps> = ({ title }) => {
+  const isTablet = useMediaQuery(`(max-width:${tabletView})`)
+  const isMobile = useMediaQuery(`(max-width:${mobileView})`)
+
   const styles = {
     container: {
-      position: 'relative' as 'relative',
+      maxWidth: desktopView,
     },
     h1: {
-      position: 'absolute' as 'absolute',
-      top: '1rem',
-      left: '2rem',
       color: 'white',
-      fontSize: '44px',
+      fontSize: isMobile ? '1rem' : isTablet ? '1.5rem' : '2rem',
+      margin: '0',
+      padding: '15px',
+      position: 'absolute' as 'absolute',
+      transform: 'translate(0%, 0%)',
+      zIndex: 1,
     },
   }
   return (
     <div style={styles.container}>
+      <h1 style={styles.h1}>{title}</h1>
       <img
         src="https://res.cloudinary.com/dejr86qx8/image/upload/v1698202248/Untitled_design_4_scxxoa.png"
         alt="mix sound"
         style={{ width: '100%' }}
       />
-      <h1 style={styles.h1}>{title}</h1>
     </div>
   )
 }
