49c109a000b4d80a549d6799ae85ffc20005f4f9
Ann <ann.suparada@gmail.com>
2024-05-22 15:21:09 -0700

    run build

--- client/src/GoogleAnalytics.tsx
+++ client/src/GoogleAnalytics.tsx
@@ -8,6 +8,7 @@ const GoogleAnalytics = () => {
     if (process.env.NODE_ENV === 'production' && trackingId) {
       ReactGA.initialize(trackingId);
       ReactGA.pageview(window.location.pathname + window.location.search);
+      console.warn('Running Google Analytics')
     } else {
       console.warn('Google Analytics is not initialized. Environment:', process.env.NODE_ENV);
     }
